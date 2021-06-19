575d29bfaf875003a7c49848a5409896fe4bb523
Joseph Panetta <jlpanetta1681@icloud.com>
2021-06-19 19:38:16 -0400

    created git repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Portfolio</h1>
+    </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>Here you will find all {props.adjective}!!</p>
+      <ul style={{ textalign: "left" }}>
+        {props.projects.map((project) => <li>{project}</li>)};
+      </ul>
+    </section >
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  )
+}
+
+const projects = [
+  "Village Book Builders",
+  "Anytime Fitness Fitness Tracker App",
+  "Human Rights First Asylum Analysis Project"
+]
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Joe" />
+      <Main adjective="My info" projects={projects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
 
 export default App;
+
