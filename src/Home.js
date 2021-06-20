ff758ecabff4411ac937049468a6a4ad796e922d
Joseph Panetta <jlpanetta1681@icloud.com>
2021-06-20 18:52:30 -0400

    lining up components

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css';
+
+
+function Home() {
+    return (
+        <div className="namebar" >
+            <div id='my-name' >
+                <h1>Joseph Panetta</h1>
+            </div>
+
+            <div className="slogan">
+                <h2>The Cutting Edge Of Web Development!!</h2>
+            </div>
+        </div>
+    )
+
+
+}
+export default Home;
