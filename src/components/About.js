3b8cc20953ab620cc3cd50cd54c62ce3a1c088a9
Joseph Panetta <jlpanetta1681@icloud.com>
2021-07-14 00:52:29 -0400

    final changes before new branch to work on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './About.css';
-
+import pic2 from '../images/mars_image.jpg'
 
 function About() {
     return (
-        <div>
+        <div className='planetPic'>
             <p>This will be where you can get to know me a little and learn some fun facts.</p>
-            <img id='important-pic' src='https://scontent-ort2-2.xx.fbcdn.net/v/t1.18169-9/547387_10151558874381737_523585170_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=dPmBDCz3akoAX_SJSke&_nc_ht=scontent-ort2-2.xx&oh=e4dd471a584e1ce7c291d67198feac01&oe=60E4899C' alt='family' />
+            <img id='interest' src={pic2} alt='mars' />
         </div>
     )
 }
